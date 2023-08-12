e94eaefb893547f4572ce4235d953c34d04fa5c7
stumurad <stugracias@gmail.com>
2023-08-11 17:40:43 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - yeb main
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	/* your code goes there */
+	char i;
+	
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+
+	return (0);
+}
