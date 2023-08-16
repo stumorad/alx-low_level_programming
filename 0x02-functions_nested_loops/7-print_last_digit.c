de95ecc321c73b2a273051e7c052c0594c871e0d
stumurad <stugracias@gmail.com>
2023-08-15 17:42:44 -0700

    m

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,15 @@ int print_last_digit(int n)
 {
 	int a;
         if (n < 0)
+	{
 		n = -n;
-        a = n % 10;
-
+	
+	a = n % 10;
+	}
 	if (a < 0)
+	{
 		a = -a;
-		_putchar(a + '0');
+	}
+	_putchar(a + '0');
 return (a);
 }
