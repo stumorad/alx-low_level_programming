f93996e8a8c0cf24210d0a5ce6632d05434201f2
stumurad <stugracias@gmail.com>
2023-08-15 17:31:01 -0700

    m

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - yeb main
+ * @n: yeb
+ * Return: 1 0 "-1"
+ */
+
+int print_sign(int n)
+{
+	if(n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
