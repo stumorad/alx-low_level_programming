a5be3c8bf331f4f27971b43e04c0be4bc326002d
stumurad <stugracias@gmail.com>
2023-08-16 15:24:06 -0700

    m

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -3,11 +3,9 @@
 /**
  * main - check the code.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-
 int main(void)
-
 {
 	print_times_table(3);
 	_putchar('\n');
@@ -15,7 +13,6 @@ int main(void)
 	_putchar('\n');
 	print_times_table(98);
 	_putchar('\n');
-	print_times_table(12);
-
+	print_times_table(12);  
 	return (0);
 }
