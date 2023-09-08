5f5648d5af9968c5fd040d3903f0785539af1b10
stumurad <stugracias@gmail.com>
2023-09-07 17:01:01 -0700

    m

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *array_range - yeb prototype
+ * @min: int
+ * @max: int
+ * Return: ptr
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+
+	return (ptr);
+}
